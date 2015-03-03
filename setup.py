233a5c9b55216b0498a95db980a7ff962c476986
Thijs Jonkman <thijs.jonkman@virtualsciences.nl>
2015-03-03 11:35:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Installer for the vs.knowledge package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt') +
+    read('docs', 'CHANGELOG.txt') +
+    read('docs', 'LICENSE.txt'))
+
+setup(
+    name='vs.knowledge',
+    version='0.1.dev0',
+    description="Knowledge Profiles",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],
+    keywords='Python Zope Plone VirtualSciences',
+    author='Virtual Sciences',
+    author_email='info@virtualsciences.nl',
+    url='https://github.com/virtualsciences/vs.knowledge',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['vs'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
---
+++ vs/knowledge/README.txt
@@ -0,0 +1,14 @@
+vs.knowledge Package Readme
+===========================
+
+Overview
+--------
+
+
+
+
+Your tests here
+---------------
+
+    >>> 1 + 1
+    3
