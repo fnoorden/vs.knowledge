18c1d3e0b561f1552bc54e0e38c138a256d98ac8
Thijs Jonkman <thijs.jonkman@virtualsciences.nl>
2015-03-16 10:06:39 +0100

    Add translations

--- vs/knowledge/__init__.py
+++ vs/knowledge/__init__.py
@@ -1,3 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+
+VSKnowledgeMessageFactory = MessageFactory('vs.knowledge')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
