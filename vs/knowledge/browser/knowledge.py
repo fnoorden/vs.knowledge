5245fc8c821ab95083be06332aeba07ea38c4603
Thijs Jonkman <thijs.jonkman@virtualsciences.nl>
2015-03-16 09:16:53 +0100

    Update templates and template logic

--- vs/knowledge/browser/knowledge.py
+++ vs/knowledge/browser/knowledge.py
@@ -1,12 +1,113 @@
+from Acquisition import aq_parent
+from collections import defaultdict
+from plone import api
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
-
+from vs.knowledge.interfaces import IKnowledgeView
 from zope.interface import implements
 
-from vs.knowledge.interfaces import IKnowledgeView
+
+class Knowledge(BrowserView):
+
+    def current(self):
+        return api.user.get_current()
+
+    def current_properties(self):
+        current = self.current()
+        c_id = current.getId()
+        cteam = current.getProperty('cteam')
+        fullname = current.getProperty('fullname')
+        return { 'id': c_id, 'cteam': cteam, 'fullname': fullname }
+
+    def current_entry(self, skill=None):
+        """
+        """
+        c_id = self.current().getId()
+        if not skill:
+            skill = self.context
+            # Check assumption and escape if necessary
+            if skill.portal_type != 'skill':
+                return (-1, (c_id, None, None))
+
+        for position, mls in enumerate(skill.member_level_show):
+            member, level, show = tuple(mls.split('|'))
+            if member == c_id:
+                return (position, (member, level, show))
+
+        return (-1, (c_id, None, None))
+
+    def knowledge_profile(self):
+        context = self.context
+        if context.portal_type == 'knowledge_profile':
+            return context
+        if context.portal_type == 'skill':
+            parent = aq_parent(context)
+            if parent.portal_type != 'knowledge_profile':
+                return False
+            return parent
+        return False
+
+    def skills(self):
+        context = self.knowledge_profile()
+        eg = context.expertises_groups
+        return sorted(
+            context.contentValues(), key=lambda x: (eg.index(x.group), x.id))
+
+    def levels(self):
+        kp = self.knowledge_profile()
+        return [l.split('|') for l in kp.levels if l.index('|') > -1]
 
 
-class KnowledgeView(BrowserView):
+class KnowledgeView(Knowledge):
     implements(IKnowledgeView)
 
-    # Utility methods
+    def add_column(self):
+        self.column_totals += [0]
+
+    def set_column_totals(self, memberorder):
+        amount = 0
+        for cteam in memberorder:
+            amount += len(cteam[1])
+        self.column_totals = [0] * amount        
+        # import sys;sys.stdout=file('/dev/stdout','w')
+        # import pdb; pdb.set_trace()
+
+    def update_column_totals(self, i, value):
+        try:
+            self.column_totals[i] += value
+        except:
+            pass
+            # import sys;sys.stdout=file('/dev/stdout','w')
+            # import pdb; pdb.set_trace()
+
+    def members(self):
+        userdict = defaultdict(list)
+        for u in sorted(api.user.get_users(), 
+                        key=lambda x: x.getProperty('fullname')):
+            cteam = u.getProperty('cteam').strip()
+            userdict[cteam if cteam else 'other'].append(u)
+        return userdict
+
+    def memberorder(self, members=None):
+        if not members:
+            members = self.members()
+        return tuple([(k, tuple([m.getId() for m in members[k]])) 
+                      for k in sorted(members.keys())])
+
+    def current_skills(self):
+        grouped_skills = defaultdict(list)
+
+        for skill in self.skills():
+            position, (member, level, show) = self.current_entry(skill)
+
+            if level:
+                group = skill.group.split('|')[1]
+                if level == 'X':
+                    grouped_skills[group].append(skill.title)
+                else:
+                    grouped_skills[group].append('%s (%s)' % (
+                        skill.title, level))
+                if ('o' not in grouped_skills or 
+                    group not in grouped_skills.get('o', [])):
+                    grouped_skills['o'].append(group)
+
+        return grouped_skills
--- vs/knowledge/browser/skill.py
+++ vs/knowledge/browser/skill.py
@@ -1,12 +1,95 @@
+from plone import api
+
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 from zope.interface import implements
 
-from ..interfaces import ISkillView
+from vs.knowledge.interfaces import ISkillView
+from vs.knowledge.browser.knowledge import Knowledge
+from vs.knowledge.vocabularies import LevelsVocabularyFactory as Levels
 
 
-class SkillView(BrowserView):
+class SkillView(Knowledge):
     implements(ISkillView)
 
-    # Utility methods
+    def update(self, position, member, level, show, values):
+        """
+        """
+
+        mls = self.context.member_level_show
+
+        new_level = values.get('level', '')
+        new_show = values.get('show', '')
+        new = u'|'.join([member, new_level, new_show])
+
+        if position < 0:
+            mls.append(new)
+        else:
+            mls[position] = new
+
+    def values(self):
+        values = self.request.form
+        position, (member, level, show) = self.current_entry()
+
+        update = values.get('update', None)
+        if update:
+            self.update(position, member, level, show, values)
+
+            directions = self.directions()
+            if update == 'Update' and directions['up']:
+                return self.request.response.redirect(directions['up'])
+            if update == '<<' and directions['back']:
+                return self.request.response.redirect(directions['back'])
+            if update == '>>' and directions['forward']:
+                return self.request.response.redirect(directions['forward'])
+
+            import sys;sys.stdout=file('/dev/stdout','w')
+            import pdb; pdb.set_trace()
+
+        return {'level': level, 'show': show}
+
+    def directions(self):
+        """
+        """
+        skills = self.skills()
+        amount = len(skills)
+
+        if not skills or amount < 2:
+            return (None, None)
+
+        index = skills.index(self.context)
+        back = skills[index-1].absolute_url() if index > 0 else ''
+        forward = skills[index+1].absolute_url() if index < (amount-1) else ''
+        up = self.knowledge_profile().absolute_url()
+        return {'up': up, 'back': back, 'forward': forward}
+
+
+    def next(self):
+        """
+        """
+
+    def update_next(self):
+        """
+        """
+
+    def previous(self):
+        """
+        """
+
+    def update_previous(self):
+        """
+        """
+
+    def status(self):
+        """
+        """
+        position, (member, level, show) = self.current_entry()
+
+        if position == -1:
+            return None
+        if level == 'X':
+            return 'x-value'
+        if level == '':
+            return 'no-value'
+        return 'level-%s' % level
